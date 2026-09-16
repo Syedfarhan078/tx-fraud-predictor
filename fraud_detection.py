405ca0ba76aac034652fe3ad60b23933bf81e356
Syed Farhan <syedfarhan3599@gmail.com>
2026-09-16 10:28:51 +0530

    Improve transaction input and validation logic
    Refactor transaction details input section and add balance validation for sender and receiver.

--- fraud_detection.py
+++ fraud_detection.py
@@ -45,36 +45,43 @@ add_bg_video("background.mp4")
 
 
 st.title("Fraud Detection Prediction App")
+st.write("Please enter the transaction details and use the predict button")
 
 model = joblib.load("fraud_detection_pipeline.pkl")
 
-st.markdown("Please enter the transaction details and use the predic button")
+col1, col2 = st.column(2)
+with col1:
+    transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASH_OUT", "DEBIT"])
+with col2:
+    amount = st.number_input("Amount", min_value=0.0, value=1000.0)
 
-st.divider()
+st.markdown("---")
+st.subheader("Sender Details")
 
-transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASH_OUT", "DEBIT"])
+col3, col4 = st.column(2)
 
-amount = st.number_input("Amount", min_value=0.0, value=1000.0)
+with col3:
+    oldbalanceOrg = st.number_input("Old Balance (Sender)", min_value=0.0, value=10000.0)
+    if amount > oldbalanceOrg:
+        st.error("You dont have sufficient balance in your account")
+with col4:
+    newbalanceOrig = st.number_input("New Balance (Sender)", min_value=0.0, value=9000.0)
+    sender_final = oldbalanceOrg - amount
+    if newbalanceOrig != sender_final:
+        st.error("Mismatch in senders old and new balance")
 
+st.markdown("---")
+st.subheader("Reciever Details")
 
-oldbalanceOrg = st.number_input("Old Balance (Sender)", min_value=0.0, value=10000.0)
-newbalanceOrig = st.number_input("New Balance (Sender)", min_value=0.0, value=9000.0)
+col5, col6 = st.columns(2)
+with col5:
+    oldbalanceDest = st.number_input("Old Balance (Receiver)", min_value=0.0, value=0.0)
+with col6:
+    newbalanceDest = st.number_input("New Balance (Receiver)", min_value=0.0, value=0.0)
+    receiver_final = oldbalanceDest + amount
 
-if amount > oldbalanceOrg:
-    st.error("You dont have sufficient balance in your account")
-
-sender_final = oldbalanceOrg - amount
-
-if newbalanceOrig != sender_final:
-    st.error("Mismatch in senders old and new balance")
-
-oldbalanceDest = st.number_input("Old Balance (Receiver)", min_value=0.0, value=0.0)
-newbalanceDest = st.number_input("New Balance (Receiver)", min_value=0.0, value=0.0)
-
-receiver_final = oldbalanceDest + amount
-
-if newbalanceDest != receiver_final:
-    st.error("Mismatch in sended and updated amount")
+    if newbalanceDest != receiver_final:
+        st.error("Mismatch in sended and updated amount")
 
 if st.button("Predict"):
     input_data = pd.DataFrame([{

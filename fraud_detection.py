452e338e26984b3068a9a93ff218838102125525
Syed Farhan <syedfarhan3599@gmail.com>
2026-09-16 10:07:31 +0530

    Implement balance checks for transaction validation
    Added balance validation checks for sender's account.

--- fraud_detection.py
+++ fraud_detection.py
@@ -56,8 +56,17 @@ transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CAS
 
 amount = st.number_input("Amount", min_value=0.0, value=1000.0)
 
+
 oldbalanceOrg = st.number_input("Old Balance (Sender)", min_value=0.0, value=10000.0)
 newbalanceOrig = st.number_input("New Balance (Sender)", min_value=0.0, value=9000.0)
+
+if amount > oldbalanceOrg:
+    st.error("You dont have sufficient balance in your account")
+
+sender_final = oldbalanceOrg - amount
+
+if newbalanceOrig != sender_final:
+    st.error("Mismatch in senders old and new balance")
 
 oldbalanceDest = st.number_input("Old Balance (Receiver)", min_value=0.0, value=0.0)
 newbalanceDest = st.number_input("New Balance (Receiver)", min_value=0.0, value=0.0)
